3d832a5e80ebf0a883f122e75a4d836f0e86c46e
Tomoya Yoshioka <tomoya.y@icloud.com>
2024-08-12 19:11:20 +0900

    Done chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,15 +7,28 @@ import { redirect } from "next/navigation"
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({ invalid_type_error: "Please select a customer." }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[]
+    amount?: string[]
+    status?: string[]
+  }
+  message?: string | null
+}
+
+export async function createInvoice(_prevState: State, formData: FormData) {
   const rawFormData = {
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
@@ -23,7 +36,16 @@ export async function createInvoice(formData: FormData) {
   }
   // Test it out:
   // console.log(rawFormData);
-  const { customerId, amount, status } = CreateInvoice.parse(rawFormData)
+  const validatedFields = CreateInvoice.safeParse(rawFormData)
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100
   const date = new Date().toISOString().split("T")[0]
 
@@ -45,12 +67,20 @@ export async function createInvoice(formData: FormData) {
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   })
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100
 
   try {
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from "@heroicons/react/24/outline"
 import Link from "next/link"
 import { Button } from "@/app/ui/button"
-import { updateInvoice } from "@/app/lib/actions"
+import { updateInvoice, State } from "@/app/lib/actions"
+import { useActionState } from "react"
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,11 +19,18 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm
   customers: CustomerField[]
 }) {
-  const updateInvoiceWithId = (formData: FormData) =>
+  const updateInvoiceWithId = (_prevState: State, formData: FormData) =>
     updateInvoice(invoice.id, formData)
 
+  const initialState: State = { message: null, errors: {} }
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState)
+
+  if (state.message) {
+    alert(state.message)
+  }
+
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -35,6 +43,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby="update-customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -46,6 +55,14 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="update-customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error) => (
+                <p key={error} className="mt-2 text-sm text-red-500">
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -64,14 +81,23 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="update-amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          <div id="update-amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error) => (
+                <p key={error} className="mt-2 text-sm text-red-500">
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
         {/* Invoice Status */}
-        <fieldset>
+        <fieldset aria-describedby="update-status-error">
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
           </legend>
@@ -110,6 +136,14 @@ export default function EditInvoiceForm({
                 </label>
               </div>
             </div>
+          </div>
+          <div id="update-status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error) => (
+                <p key={error} className="mt-2 text-sm text-red-500">
+                  {error}
+                </p>
+              ))}
           </div>
         </fieldset>
       </div>
