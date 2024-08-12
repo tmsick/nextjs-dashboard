8405e5fa2bc6d40989febb885a7e32dd54293b27
Tomoya Yoshioka <tomoya.y@icloud.com>
2024-08-12 20:14:04 +0900

    Done chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -110,3 +110,25 @@ export async function deleteInvoice(id: string) {
 
   revalidatePath("/dashboard/invoices")
 }
+
+import { signIn } from "@/auth"
+import { AuthError } from "next-auth"
+
+export async function authenticate(
+  _prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid Credentials."
+        default:
+          return "Something went wrong."
+      }
+    }
+    throw error
+  }
+}
---
+++ auth.config.ts
@@ -0,0 +1,26 @@
+import type { NextAuthConfig } from "next-auth"
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard")
+
+      if (isOnDashboard) {
+        return isLoggedIn
+      }
+
+      if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl))
+      }
+
+      return true
+    },
+  },
+  providers: [
+    /* TODO */
+  ],
+} satisfies NextAuthConfig
